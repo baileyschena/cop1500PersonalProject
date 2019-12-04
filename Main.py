60ccd2ec5960760deb84d1ada85b117081f0642e
baileyschena <baileyschena@gmail.com>
2019-12-03 23:51:51 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,17 +1,17 @@
-#Personal Project by Bailey Schena
-#Calculator
+# Personal Project by Bailey Schena
+# Calculator
 
-#Taking in a number 
+# Taking in a number
 print("Please enter the first number")
 numberOneInput = input("")
 numberOne = int(numberTwoInput)
 
-#Taking in a second number 
+# Taking in a second number
 print("Please enter the second number")
 numberTwoInput = input("")
 numberTwo = int(numberTwoInput)
 
-#Choosing the operation
+# Choosing the operation
 print("Please select an operation to perform with the two numbers.")
 print("1. Add")
 print("2. Multiply")
@@ -22,15 +22,14 @@ userInput = int(userInputNum)
 
 outputAnswer = 0
 
-if(userInput == 1):
+if userInput == 1:
     print("The answer is: ", numberOne + numberTwo)
 
-else if(userInput == 2):
+elif userInput == 2:
     print("The answer is: ", numberOne * numberTwo)
 
-else if(userInput == 3):
-    print("The answer is: ", numberOne - numberTwo)  
+elif userInput == 3:
+    print("The answer is: ", numberOne - numberTwo)
 
-else if(userInput == 4):
+elif userInput == 4:
     print("The answer is: ", numberOne / numberTwo)
-#Going to change this program completely into something a bit more useful.
